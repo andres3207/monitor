a7fc4605962563824778069fdbd38d671977e10e
Andres <andres3207@gmail.com>
2016-11-23 08:31:14 -0300

    Ahora reporta a los usuarios

--- application/third_party/scripts/temphum.py
+++ application/third_party/scripts/temphum.py
@@ -2,6 +2,7 @@
 
 import MySQLdb
 import random
+import telepot
 
 DB_HOST='localhost'
 DB_USER='root'
@@ -37,6 +38,68 @@ file2.close()
 #hum=random.randrange(100)
 
 query="INSERT INTO datos (temperatura, humedad) VALUES ('%s','%s')" %(temp,hum)
-
 print query
 run_query(query)
+
+query="SELECT * from configuracion where 1"
+datos=run_query(query)
+
+t_min=datos[0][1]
+t_max=datos[0][2]
+h_min=datos[0][3]
+h_max=datos[0][4]
+
+if (temp<t_min):
+   if(hum<h_min):
+      reporte="temperatura y humedad por debajo"
+      reporte="TEMPERATURA = "+temp+" por debajo del limite configurado de "+str(t_min)+". HUMEDAD = "+hum+" por debajo del limite configurado de "+str(h_min)+"."
+      condicion=0
+   elif((hum>=h_min) and (hum<=h_max)):
+      reporte="temperatura por debajo, humedad ok"
+      reporte="TEMPERATURA = "+temp+" por debajo del limite configurado de "+str(t_min)+". HUMEDAD = "+hum+" dentro del limite configurado de "+str(h_min)+" y "+str(h_max)+"."
+      condicion=1
+   elif(hum>h_max):
+      reporte="temperatura por debajo, humedad por encima"
+      reporte="TEMPERATURA = "+temp+" por debajo del limite configurado de "+str(t_min)+". HUMEDAD = "+hum+" por encima del limite configurado de "+str(h_min)+"."
+      condicion=2
+elif((temp>=t_min) and (temp<=t_max)):
+   if(hum<h_min):
+      reporte="temperatura ok, humedad por debajo"
+      reporte="TEMPERATURA = "+temp+" dentro del limite configurado de "+str(t_min)+" y "+str(t_max)+". HUMEDAD = "+hum+" por debajo del limite configurado de "+str(h_min)+"."
+      condicion=3
+   elif((hum>=h_min) and (hum<=h_max)):
+      reporte="temperatura ok, humedad ok"
+      reporte="TEMPERATURA = "+temp+" dentro del limite configurado de "+str(t_min)+" y "+str(t_max)+". HUMEDAD = "+hum+" dentro del limite configurado de "+str(h_min)+" y "+str(h_max)+"."
+      condicion=4
+   elif(hum>h_max):
+      reporte="temperatura ok, humedad por encima"
+      reporte="TEMPERATURA = "+temp+" dentro del limite configurado de "+str(t_min)+" y "+str(t_max)+". HUMEDAD = "+hum+" por encima del limite configurado de "+str(h_min)+"."
+      condicion=5
+elif (temp>t_max):
+   if(hum<h_min):
+      reporte="temperatura por encima, humedad por debajo"
+      reporte="TEMPERATURA = "+temp+" por encima del limite configurado de "+str(t_max)+". HUMEDAD = "+hum+" por debajo del limite configurado de "+str(h_min)+"."
+      condicion=6
+   elif((hum>=h_min) and (hum<=h_max)):
+      reporte="temperatura por encima, humedad ok"
+      reporte="TEMPERATURA = "+temp+" por encima del limite configurado de "+str(t_max)+". HUMEDAD = "+hum+" dentro del limite configurado de "+str(h_min)+" y "+str(h_max)+"."
+      condicion=7
+   elif(hum>h_max):
+      reporte="temperatura por encima, humedad por encima"
+      reporte="TEMPERATURA = "+temp+" por encima del limite configurado de "+str(t_max)+". HUMEDAD = "+hum+" por encima del limite configurado de "+str(h_min)+"."
+      condicion=8
+
+query="INSERT INTO alertas (mensaje) VALUES ('%s')" %(reporte)
+#print query
+run_query(query)
+
+query="SELECT chat_id from usuarios where habilitado=1"
+destinatarios=run_query(query)
+
+#print len(destinatarios)
+bot=telepot.Bot('196708475:AAFXMiVQVR1CwyYcs9Hv4Lsa1otAg4gLCM0')
+
+for i in range(len(destinatarios)):
+   chat_id=str(destinatarios[i][0])
+   #print chat_id
+   bot.sendMessage(chat_id,reporte)
