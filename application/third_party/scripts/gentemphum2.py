06c1d01598cf8d08423da07433c5c76e3a8b04d3
Andres <andres3207@gmail.com>
2016-12-13 09:57:59 -0300

    Cambios visuales y reportes constantemente

--- application/third_party/scripts/gentemphum2.py
+++ application/third_party/scripts/gentemphum2.py
@@ -6,15 +6,64 @@ import sys
 
 import Adafruit_DHT
 
+import MySQLdb
+import telepot
+
+from datetime import datetime
+
 
 
 
 sensor = 22
 pin = 22
 
+condicon=4
+
+periodo_sin_alarma=60   #En segundos
+
 
 #print "Temp: "+temp
 #print "Hum: "+hum
+
+
+DB_HOST='localhost'
+DB_USER='root'
+DB_PASS='andres'
+DB_NAME='monitoreo'
+
+
+def run_query(query=''):
+   datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
+   conn = MySQLdb.connect(*datos) # Conectar a la base de datos 
+   cursor = conn.cursor()         # Crear un cursor 
+   cursor.execute(query)          # Ejecutar una consulta
+   if query.upper().startswith('SELECT'):
+      data = cursor.fetchall()   # Traer los resultados de un select 
+   else:
+      conn.commit()              # Hacer efectiva la escritura de datos 
+      data = None 
+   
+   cursor.close()                 # Cerrar el cursor 
+   conn.close()                   # Cerrar la conexion 
+
+   return data
+
+
+def enviar_alertas(reporte,condicion):
+   query="INSERT INTO alertas (mensaje,condicion) VALUES ('%s','%s')" %(reporte,condicion)
+   #print query
+   run_query(query)
+   
+   query="SELECT chat_id from usuarios where habilitado=1"
+   destinatarios=run_query(query)
+   
+   #print len(destinatarios)
+   bot=telepot.Bot('196708475:AAFXMiVQVR1CwyYcs9Hv4Lsa1otAg4gLCM0')
+   
+   for i in range(len(destinatarios)):
+      chat_id=str(destinatarios[i][0])
+      #print chat_id
+      bot.sendMessage(chat_id,reporte)
 
 
 while(1):
@@ -24,6 +73,75 @@ while(1):
    if humidity is not None and temperature is not None:
       temp="{0:0.1f}".format(temperature)
       hum="{0:0.1f}".format(humidity)
+
+      query="SELECT * from configuracion where 1"
+      datos=run_query(query)
+
+      t_min=datos[0][1]
+      t_max=datos[0][2]
+      h_min=datos[0][3]
+      h_max=datos[0][4]
+
+      if (temp<t_min):
+         if(hum<h_min):
+            reporte="temperatura y humedad por debajo"
+            reporte="TEMPERATURA = "+temp+" por debajo del limite configurado de "+str(t_min)+". HUMEDAD = "+hum+" por debajo del limite configurado de "+str(h_min)+"."
+            condicion=0
+         elif((hum>=h_min) and (hum<=h_max)):
+            reporte="temperatura por debajo, humedad ok"
+            reporte="TEMPERATURA = "+temp+" por debajo del limite configurado de "+str(t_min)+". HUMEDAD = "+hum+" dentro del limite configurado de "+str(h_min)+" y "+str(h_max)+"."
+            condicion=1
+         elif(hum>h_max):
+            reporte="temperatura por debajo, humedad por encima"
+            reporte="TEMPERATURA = "+temp+" por debajo del limite configurado de "+str(t_min)+". HUMEDAD = "+hum+" por encima del limite configurado de "+str(h_min)+"."
+            condicion=2
+      elif((temp>=t_min) and (temp<=t_max)):
+         if(hum<h_min):
+            reporte="temperatura ok, humedad por debajo"
+            reporte="TEMPERATURA = "+temp+" dentro del limite configurado de "+str(t_min)+" y "+str(t_max)+". HUMEDAD = "+hum+" por debajo del limite configurado de "+str(h_min)+"."
+            condicion=3
+         elif((hum>=h_min) and (hum<=h_max)):
+            reporte="temperatura ok, humedad ok"
+            reporte="TEMPERATURA = "+temp+" dentro del limite configurado de "+str(t_min)+" y "+str(t_max)+". HUMEDAD = "+hum+" dentro del limite configurado de "+str(h_min)+" y "+str(h_max)+"."
+            condicion=4
+         elif(hum>h_max):
+            reporte="temperatura ok, humedad por encima"
+            reporte="TEMPERATURA = "+temp+" dentro del limite configurado de "+str(t_min)+" y "+str(t_max)+". HUMEDAD = "+hum+" por encima del limite configurado de "+str(h_min)+"."
+            condicion=5
+      elif (temp>t_max):
+         if(hum<h_min):
+            reporte="temperatura por encima, humedad por debajo"
+            reporte="TEMPERATURA = "+temp+" por encima del limite configurado de "+str(t_max)+". HUMEDAD = "+hum+" por debajo del limite configurado de "+str(h_min)+"."
+            condicion=6
+         elif((hum>=h_min) and (hum<=h_max)):
+            reporte="temperatura por encima, humedad ok"
+            reporte="TEMPERATURA = "+temp+" por encima del limite configurado de "+str(t_max)+". HUMEDAD = "+hum+" dentro del limite configurado de "+str(h_min)+" y "+str(h_max)+"."
+            condicion=7
+         elif(hum>h_max):
+            reporte="temperatura por encima, humedad por encima"
+            reporte="TEMPERATURA = "+temp+" por encima del limite configurado de "+str(t_max)+". HUMEDAD = "+hum+" por encima del limite configurado de "+str(h_min)+"."
+            condicion=8
+
+      if(condicion!=4):
+         query="SELECT MAX(id) FROM alertas WHERE 1"
+         n=run_query(query)
+         n=n[0][0]
+
+         query="SELECT cuando,condicion FROM alertas WHERE id="+str(n)
+         resultado=run_query(query)
+         fecha=resultado[0][0]
+         ultima_condicion=resultado[0][1]
+         #print fecha
+         #print ultima_condicion
+         ahora=datetime.now()
+         delta = ahora - fecha
+         #print delta
+         diferencia=delta.total_seconds()
+
+         if((ultima_condicion!=condicion) or (diferencia>=periodo_sin_alarma)):
+            enviar_alertas(reporte,condicion)
+
+
       f=open("/var/www/web/monitor/application/third_party/scripts/temp","w")
       f.write(temp)
       f.close()
